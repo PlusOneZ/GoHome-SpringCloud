8c9671c44b20963afc6d74383fda768c485300c4
plusone <zhuozhengyi@icloud.com>
2021-11-16 14:55:20 +0800

    Initialize a Spring Cloud Project

---
+++ eureka/src/test/java/cn/edu/tongji/gohome/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.gohome.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ login/src/test/java/cn/edu/tongji/gohome/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.gohome.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
