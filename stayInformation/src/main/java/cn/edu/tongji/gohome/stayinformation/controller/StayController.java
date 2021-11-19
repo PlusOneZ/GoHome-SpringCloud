c9eb45d2f39a9ab360d2cddad5a15fae22d4cd73
汪汪汪 <1209497940@qq.com>
2021-11-19 18:58:55 +0800

    init the first api for stay

---
+++ stayInformation/src/main/java/cn/edu/tongji/gohome/stayinformation/controller/StayController.java
@@ -0,0 +1,17 @@
+package cn.edu.tongji.gohome.stayinformation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 和房源信息查询相关的api
+ *
+ * @author 汪明杰
+ * @date 2021/11/19 18:57
+ */
+
+@RestController
+@RequestMapping("api/v1/stay")
+public class StayController {
+    
+}
