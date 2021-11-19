6538594257bb780c5b82afa8589263ffed0ef13a
plusone <zhuozhengyi@icloud.com>
2021-11-19 11:16:43 +0800

    encrypt the db password

--- login/src/test/java/cn/edu/tongji/gohome/login/LoginApplicationTests.java
+++ login/src/test/java/cn/edu/tongji/gohome/login/LoginApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.edu.tongji.gohome.login;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,21 @@ class LoginApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void encryptPassword() {
+        StandardPBEStringEncryptor standardPBEStringEncryptor =new StandardPBEStringEncryptor();
+        /*配置文件中配置如下的算法*/
+        standardPBEStringEncryptor.setAlgorithm("PBEWithMD5AndDES");
+        /*配置文件中配置的password*/
+        standardPBEStringEncryptor.setPassword("kao^QUan^yaNG^a^En");
+        /*要加密的文本*/
+        String name = standardPBEStringEncryptor.encrypt("这里写用户名");
+        String password =standardPBEStringEncryptor.encrypt("这里写密码");
+        /*将加密的文本写到配置文件中*/
+        System.out.println("name="+name);
+        System.out.println("password="+password);
     }
 
 }
