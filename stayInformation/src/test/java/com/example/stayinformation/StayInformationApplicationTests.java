42ac4ad0fb557dbfca7793cb336434ac7d824d64
汪汪汪 <1209497940@qq.com>
2021-11-19 10:04:18 +0800

    新建了stayInformation模块

---
+++ stayInformation/src/test/java/com/example/stayinformation/StayInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stayinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayInformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
