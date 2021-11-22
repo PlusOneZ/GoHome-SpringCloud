fa35f8a5e663e698531f44f29d577782306276b8
plusone <zhuozhengyi@icloud.com>
2021-11-22 18:23:34 +0800

    Create a gateway module

---
+++ gateway/src/main/java/cn/edu/tongji/gohome/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.gohome.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
