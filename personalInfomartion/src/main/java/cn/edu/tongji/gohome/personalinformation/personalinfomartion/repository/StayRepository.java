eaac97835d610a8fe97cd1a78d3da79c03d7e133
Qiao Liang <19921305202@163.com>
2021-11-23 21:41:25 +0800

    Complete the first api
    Complete the first api of the user information microservice: obtain the basic information of the user's homepage

---
+++ personalInfomartion/src/main/java/cn/edu/tongji/gohome/personalinformation/personalinfomartion/repository/StayRepository.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.gohome.personalinformation.personalinfomartion.repository;
+
+import cn.edu.tongji.gohome.personalinformation.personalinfomartion.model.StayEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * StayRepository类的DAO
+ * @author 梁乔
+ * @since 2021/11/23 16:52
+ */
+public interface StayRepository extends JpaRepository<StayEntity, Long> {
+    StayEntity findByStayId(Long stayId);
+}
