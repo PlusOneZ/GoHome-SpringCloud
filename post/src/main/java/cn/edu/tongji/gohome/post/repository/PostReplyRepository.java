b03fb97fe8fd5b8847897e9ff306084514878efe
Letian <54671931+li-letian@users.noreply.github.com>
2021-11-28 02:04:50 +0800

    finish apis about reply

--- post/src/main/java/cn/edu/tongji/gohome/post/repository/PostReplyRepository.java
+++ post/src/main/java/cn/edu/tongji/gohome/post/repository/PostReplyRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostReplyRepository extends JpaRepository<PostReplyEntity, Long>, JpaSpecificationExecutor<PostReplyEntity> {
+
+
+    @Query("select p from PostReplyEntity p where p.preReplyId is null and p.postId = ?1")
     Page<PostReplyEntity> findAllByPostId(long postId, Pageable pageable);
 
+    Page<PostReplyEntity> findAllByPreReplyId(long replyId, Pageable pageable);
 }
--- post/src/main/java/cn/edu/tongji/gohome/post/service/impl/ReplyServiceImpl.java
+++ post/src/main/java/cn/edu/tongji/gohome/post/service/impl/ReplyServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.edu.tongji.gohome.post.service.impl;
 
-import cn.edu.tongji.gohome.post.model.PostEntity;
+import cn.edu.tongji.gohome.post.dto.PostCustomer;
+import cn.edu.tongji.gohome.post.dto.mapper.PostCustomerMapper;
 import cn.edu.tongji.gohome.post.model.PostReplyEntity;
+import cn.edu.tongji.gohome.post.repository.CustomerRepository;
 import cn.edu.tongji.gohome.post.repository.PostReplyRepository;
 import cn.edu.tongji.gohome.post.service.PostService;
 import cn.edu.tongji.gohome.post.service.ReplyService;
@@ -9,10 +11,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
-import java.util.List;
 
 
 @Service
@@ -24,6 +26,32 @@ public class ReplyServiceImpl implements ReplyService {
     @Resource
     private PostReplyRepository postReplyRepository;
 
+    @Resource
+    private CustomerRepository customerRepository;
+
+    private Page<HashMap<String,Object>> recursiveSearchComment(Page<PostReplyEntity> replyEntityList)
+    {
+
+        if(replyEntityList.isEmpty())
+        {
+            return null;
+        }
+
+        Page<HashMap<String,Object>> replyList=replyEntityList.map(postReplyEntity -> {
+            PostCustomer customer= PostCustomerMapper.getInstance().toDto(customerRepository.findOneByCustomerId(postReplyEntity.getCustomerId()));
+            HashMap<String,Object> sonReply=searchSonReplyListForReplyId(postReplyEntity.getReplyId(),0,8);
+            HashMap<String,Object> result=new HashMap<>();
+            result.put("reply",postReplyEntity);
+            result.put("customer",customer);
+            result.put("sonReply",sonReply);
+            return result;
+        });
+
+        return replyList;
+
+    }
+
+
     @Override
     public HashMap<String, Object> searchPostReplyListForPostId(long postId, int currentPage, int pageSize) {
 
@@ -33,14 +61,34 @@ public class ReplyServiceImpl implements ReplyService {
 
         Page<PostReplyEntity> replyEntityList = postReplyRepository.findAllByPostId(postId,pageable);
 
-        results.put("postInfo", replyEntityList);
+        Object obj=recursiveSearchComment(replyEntityList);
 
+        if(obj==null)
+        {
+            return null;
+        }
+
+        results.put("replyInfo", obj);
         return results;
-
     }
 
     @Override
     public HashMap<String, Object> searchSonReplyListForReplyId(long replyId, int currentPage, int pageSize) {
-        return null;
+
+        Pageable pageable= PageRequest.of(currentPage, pageSize);
+
+        HashMap<String, Object> results = new HashMap<>();
+
+        Page<PostReplyEntity> replyEntityList = postReplyRepository.findAllByPreReplyId(replyId,pageable);
+
+        Object obj=recursiveSearchComment(replyEntityList);
+
+        if(obj==null)
+        {
+            return null;
+        }
+
+        results.put("replyInfo", obj);
+        return results;
     }
 }
