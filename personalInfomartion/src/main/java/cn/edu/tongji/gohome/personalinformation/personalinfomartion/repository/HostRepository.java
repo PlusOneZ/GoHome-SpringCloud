71682ad0c4273328c2eb321ebf6774aa87d27f6c
Qiao Liang <19921305202@163.com>
2021-11-29 21:28:44 +0800

    complete 3 apis of personalinformation service

--- personalInfomartion/src/main/java/cn/edu/tongji/gohome/personalinformation/personalinfomartion/repository/HostRepository.java
+++ personalInfomartion/src/main/java/cn/edu/tongji/gohome/personalinformation/personalinfomartion/repository/HostRepository.java
@@ -10,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 */
 public interface HostRepository extends JpaRepository<HostEntity, Integer> {
     HostEntity findByHostId(Integer hostId);
+    HostEntity findByCustomerId(Long customerId);
 }
