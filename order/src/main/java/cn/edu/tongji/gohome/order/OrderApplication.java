2a461dc7fe1de0cc9097277057df0d62aa678fe3
llleoy <1421877537@qq.com>
2021-11-29 18:43:22 +0800

    modify get order Information API for "order" micro service

--- order/src/main/java/cn/edu/tongji/gohome/order/OrderApplication.java
+++ order/src/main/java/cn/edu/tongji/gohome/order/OrderApplication.java
@@ -3,6 +3,9 @@ package cn.edu.tongji.gohome.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,25 @@ public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:8080",
+                                "http://*.guisu.website",
+                                "https://*.guisu.website"
+                        )
+                        .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 
 }
