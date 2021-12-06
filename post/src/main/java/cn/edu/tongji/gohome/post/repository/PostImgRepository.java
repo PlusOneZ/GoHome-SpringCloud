a063f69a343d986acb193c08da01a6c05111c9c0
Letian <54671931+li-letian@users.noreply.github.com>
2021-12-07 00:29:17 +0800

    fix bug about post image

--- post/src/main/java/cn/edu/tongji/gohome/post/repository/PostImgRepository.java
+++ post/src/main/java/cn/edu/tongji/gohome/post/repository/PostImgRepository.java
@@ -3,11 +3,13 @@ package cn.edu.tongji.gohome.post.repository;
 import cn.edu.tongji.gohome.post.model.PostImgEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostImgRepository extends JpaRepository<PostImgEntity, Long>, JpaSpecificationExecutor<PostImgEntity> {
     List<PostImgEntity> findAllByPostId(long postId);
 
+    @Query("select distinct p.postImgLink from PostImgEntity p where p.postId = ?1")
     List<String> findDistinctByPostId(long postId);
 }
